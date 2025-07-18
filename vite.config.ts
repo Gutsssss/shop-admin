4c6dcc2436668e1ada7b07225a7a968f7526c1d0
Matvey M <kacill1@mail.ru>
2025-07-18 20:00:59 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,50 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {resolve} from 'path'
+import { resolve } from 'path'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': {},
+    global: 'globalThis',
   },
   resolve: {
-      alias: [
-        {find:'@components',replacement:resolve(__dirname, './src/components')},
-        {find:'@hooks',replacement:resolve(__dirname, './src/hooks')},
-        {find:'@pages',replacement:resolve(__dirname, './src/pages')},
-        {find:'@store',replacement:resolve(__dirname,'./src/store')},
-        {find:'@',replacement:resolve(__dirname,'./src')}
-        ]
+    alias: [
+      { find: '@components', replacement: resolve(__dirname, './src/components') },
+      { find: '@hooks', replacement: resolve(__dirname, './src/hooks') },
+      { find: '@pages', replacement: resolve(__dirname, './src/pages') },
+      { find: '@store', replacement: resolve(__dirname, './src/store') },
+      { find: '@', replacement: resolve(__dirname, './src') }
+    ],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin()
+      ],
     },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        rollupNodePolyFill()
+      ],
+      external: [
+        'crypto'
+      ]
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    }
+  }
 })
